fbc18f7a7b701d60c8af8fad0ef1d1f442dde2aa
Harsh-620 <89959976+Harsh-620@users.noreply.github.com>
2023-12-30 16:01:52 +0530

    Update App.jsx

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -3,38 +3,36 @@ import { useState, useCallback, useEffect, useRef } from 'react'
 
 
 function App() {
-  const [length, setLength] = useState(8)
+  const [length, setLength] = useState(8)           //Here we stored all the variables in state because if they are changed re rendering should occure
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false)
   const [password, setPassword] = useState("")
 
   //useRef hook
-  const passwordRef = useRef(null)
+  const passwordRef = useRef(null)  
 
-  const passwordGenerator = useCallback(() => {
+  const passwordGenerator = useCallback(() => {     //we used a optimised approach by using call back to genefrate password.We could have used normal function also.
     let pass = ""
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-    if (numberAllowed) str += "0123456789"
-    if (charAllowed) str += "!@#$%^&*-_+=[]{}~`"
+    if (numberAllowed) str += "0123456789"      //If numberallowed box is ticked then 
+    if (charAllowed) str += "!@#$%^&*-_+=[]{}~`" //if character allowed box is ticked then
 
-    for (let i = 1; i <= length; i++) {
-      let char = Math.floor(Math.random() * str.length + 1)
+    for (let i = 1; i <= length; i++) {   //it generates the password.
+      let char = Math.floor(Math.random() * str.length + 1)  
       pass += str.charAt(char)
       
     }
 
-    setPassword(pass)
+    setPassword(pass)   //Here password gets updated
+ }, [length, numberAllowed, charAllowed, setPassword])
 
-
-  }, [length, numberAllowed, charAllowed, setPassword])
-
-  const copyPasswordToClipboard = useCallback(() => {
-    passwordRef.current?.select();
-    passwordRef.current?.setSelectionRange(0, 999);
-    window.navigator.clipboard.writeText(password)
+  const copyPasswordToClipboard = useCallback(() => {   //we could have used normal function here but again for optimisation that if password remains same then it will store the function
+    passwordRef.current?.select(); //it helps in selecting the copied password
+    passwordRef.current?.setSelectionRange(0, 999);//It gives the range of selection 
+    window.navigator.clipboard.writeText(password) //It helps in coying password to clipboard
   }, [password])
 
-  useEffect(() => {
+  useEffect(() => {   //It helps in calling the passwordgenerator function.If we will call it normally it will not work
     passwordGenerator()
   }, [length, numberAllowed, charAllowed, passwordGenerator])
   return (
@@ -48,10 +46,10 @@ function App() {
             className="outline-none w-full py-1 px-3"
             placeholder="Password"
             readOnly
-            ref={passwordRef}
+            ref={passwordRef}  //here we used the created varibale with help of useRef hook so that the password get stored in the current value of that reference. 
         />
         <button
-        onClick={copyPasswordToClipboard}
+        onClick={copyPasswordToClipboard}  //here eventlistener got added and a reference of copyPasswordToClipboard is passed.Note that it is not called here.
         className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
         >copy</button>
         
